28050673c0a039b3ecf2be3dbc14bc4e32ad3077
MICHAEL WATTS <m_watts@cda074ev5815x.local>
2018-11-22 16:20:31 -0500

    P3 - Setp up background and main image

--- exercises/project3/js/script.js
+++ exercises/project3/js/script.js
@@ -8,11 +8,19 @@ author, and this description to match your project!
 
 ******************/
 
+var mural;
+var muscle;
+var halo;
+
 // preload()
 //
 // Description of preload
 
+// Set up canvas called "mural"
 function preload() {
+  mural = loadImage("assets/images/mural.png");
+  muscle = loadImage("assets/images/muscle.png");
+  halo = loadImage("assets/images/halo.png");
 
 }
 
@@ -22,6 +30,10 @@ function preload() {
 // Description of setup
 
 function setup() {
+// Create canvas
+createCanvas(windowWidth,windowHeight);
+imageMode(CENTER);
+
 
 }
 
@@ -31,5 +43,13 @@ function setup() {
 // Description of draw()
 
 function draw() {
+// create mural to frame game
+image(mural,width/2,height/2,windowWidth,windowHeight);
+image(halo,width/2-10,height/2-170);
+image(muscle,width/2,height/2);
+}
 
+function windowResized() {
+  // We can use the resizeCanvas() function to resize our canvas to the new window dimensions
+  resizeCanvas(windowWidth,windowHeight);
 }
