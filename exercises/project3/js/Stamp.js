a410840ff758bbd7e2af705e3f7f50c6be42214e
MICHAEL WATTS <m_watts@cda074ev5815x.local>
2018-11-22 17:49:05 -0500

    P3 - set up stamp class with mouse pressed

---
+++ exercises/project3/js/Stamp.js
@@ -0,0 +1,22 @@
+// Stamp
+//
+// A class to define how the stamps behaves. This will include when they appear
+// on the screen and how they are moved into a selected position.
+
+// Halo constructor
+//
+// Sets the properties with the provided arguments
+function Stamp(x,y,stampImage) {
+  this.x = x;
+  this.y = y;
+  //this.size = size;
+  this.stampImage = stampImage;
+  }
+
+  Stamp.prototype.update = function () {
+  }
+
+  // Draw the halo as an image on screen
+  Stamp.prototype.display = function () {
+    image(this.stampImage,this.x,this.y);
+  }
--- exercises/project3/js/script.js
+++ exercises/project3/js/script.js
@@ -1,27 +1,31 @@
 /*****************
 
-Title of Project
-Author Name
+Stamp
+Created by Michael Watts
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+// Artistic collage using random images/stamps that appear and the user
+// can select where on the canvas they want the stamps to appear
 
 ******************/
-
+// Variables to contain the canvas and stamps that will be
+// used during the experience
 var mural;
 var muscle;
-var halo;
+
+var haloImage;
+var haloStamp;
+var stamped = false;
+
 
 // preload()
 //
 // Description of preload
 
-// Set up canvas called "mural"
+// Set up canvas called "mural" along with images for collage
 function preload() {
   mural = loadImage("assets/images/mural.png");
   muscle = loadImage("assets/images/muscle.png");
-  halo = loadImage("assets/images/halo.png");
-
+  haloImage = loadImage("assets/images/halo.png");
 }
 
 
@@ -30,11 +34,12 @@ function preload() {
 // Description of setup
 
 function setup() {
-// Create canvas
-createCanvas(windowWidth,windowHeight);
-imageMode(CENTER);
+  // Create canvas
+  createCanvas(windowWidth,windowHeight);
+  imageMode(CENTER);
 
-
+  // Create halo stamp
+  haloStamp = new Stamp(width/2,height/2,haloImage);
 }
 
 
@@ -43,12 +48,29 @@ imageMode(CENTER);
 // Description of draw()
 
 function draw() {
-// create mural to frame game
-image(mural,width/2,height/2,windowWidth,windowHeight);
-image(halo,width/2-10,height/2-170);
-image(muscle,width/2,height/2);
+  // background for display
+  background (255, 255, 255);
+
+  // Sets up the conditions for the stamps to follow mouse and
+  // be stamped when mouse is pressed 
+  if (stamped === false) {
+    haloStamp.x = mouseX;
+    haloStamp.y = mouseY;
+  }
+  // set up display for stamps
+  haloStamp.display ();
+
+  // create mural to frame game and muscle image as main starting piece
+  image(muscle,width/2,height/2);
+  image(mural,width/2,height/2,windowWidth,windowHeight);
 }
 
+// Calls when mouse is pressed to stamp image
+function mousePressed() {
+  stamped = true;
+}
+
+// set up canvas size to fit in window
 function windowResized() {
   // We can use the resizeCanvas() function to resize our canvas to the new window dimensions
   resizeCanvas(windowWidth,windowHeight);
